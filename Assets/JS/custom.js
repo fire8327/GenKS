e34ea4ae7622a8c4ed43d7ac9d580c56bc9db364
fire8327 <fire83274@gmail.com>
2023-04-27 00:06:01 +0300

    upd2

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -5,3 +5,19 @@ $("#toggler1, #toggler2").click(() => {
   $("#toggler1").toggleClass("text-white");
   $("#toggler2").toggleClass("text-white");
 });
+$("#toggler1").click(()=>{
+  $(".sum").each((i,el)=>{
+    $(el).find(".price").text("1 845 ₽");
+    $(el).find(".month").find(".year").toggleClass("hidden");
+  });
+  $(".sum2").find(".price").text("9 235 ₽");
+  $(".sum2").find(".month").find(".year").toggleClass("hidden");
+});
+$("#toggler2").click(()=>{
+  $(".sum").each((i,el)=>{
+    $(el).find(".price").text("1 582 ₽");
+    $(el).find(".month").find(".year").toggleClass("hidden");
+  });
+  $(".sum2").find(".price").text("7 916 ₽");
+  $(".sum2").find(".month").find(".year").toggleClass("hidden");
+});
