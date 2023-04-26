3af7b106cd2b6e986d78d33e7042738877601073
fire8327 <fire83274@gmail.com>
2023-04-26 23:54:17 +0300

    upd

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,1 +1,7 @@
 /*jshint esnext: true */
+$("#toggler1, #toggler2").click(() => {
+  $("#toggle").toggleClass("-translate-x-[calc(100%-14px)]");
+  $("#toggle").toggleClass("w-1/2");
+  $("#toggler1").toggleClass("text-white");
+  $("#toggler2").toggleClass("text-white");
+});
